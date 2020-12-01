81053ca94835dc0a53941998bd0aabb06efa9773
LiquidFunki <coolklimenko@gmail.com>
2020-12-01 05:02:35 -0500

    lab07

---
+++ lab07/src/6.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#define SIZE 3
+
+int mas_func(int massive[SIZE][SIZE]);
+
+int main()
+{
+    srand(time(NULL));
+    int massive[SIZE][SIZE];
+        for(int i = 0; i <= SIZE; i++)
+        {
+            for(int j = 0; j <= SIZE; j++){
+                massive[i][j] = rand() % 10;
+            }
+        }
+    int result = mas_func(massive);
+    return 0;
+}
+
+
+
+int mas_func(int massive[SIZE][SIZE])
+{
+    int new_massive[SIZE][SIZE] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+              for (int k = 0; k < SIZE; k++)
+              {
+                new_massive[i][j] += massive[i][k] * massive[k][j];
+              }
+        }
+    }
+    return new_massive;
+}
+
+
+
